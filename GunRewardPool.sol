141ab3bbbb8b3db7f18a6f61422f24709d88b345
FLEXTRA <81846963+FlextraApe@users.noreply.github.com>
2022-10-26 01:03:45 +1000

    Update GunRewardPool.sol

--- GunRewardPool.sol
+++ GunRewardPool.sol
@@ -21,7 +21,7 @@ interface IERC20 {
      * @dev Returns the amount of tokens in existence.
      */
     function totalSupply() external view returns (uint256);
-
+  
     /**
      * @dev Returns the amount of tokens owned by `account`.
      */
